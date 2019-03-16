c5d5cd5863546048fae8afdeda8b9711269ab896
Nandor Gallo <nandor.gallo@uoit.net>
2019-03-16 13:59:58 -0400

    added SnakeGame, Fixed Pong

--- src/Pong.java
+++ src/Pong.java
@@ -152,9 +152,6 @@ public class Pong extends Application{
 		        	}else{
 		        		Paddle1.setY(Paddle1.getY() - DELTA_Y);
 		        	}break;
-		        case R:
-		        	reset(ball);
-		        	break;
 		        case SPACE:
 		        	//When Space key is pressed Start The movement and Timer
 		        	Board.getChildren().remove(prompt);
@@ -173,7 +170,6 @@ public class Pong extends Application{
 		        	break;
 		      }
 		    });
-		
 	}
 	public void reset(Circle c){
 		Random rand = new Random();
